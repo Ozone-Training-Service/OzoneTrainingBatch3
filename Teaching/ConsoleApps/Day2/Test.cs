b9616424920675aed8c930d01807f59c590fc6c4
vishnupc <66262226+vishnupc0016@users.noreply.github.com>
2020-07-17 11:59:42 +0530

    change

--- Teaching/ConsoleApps/Day2/Test.cs
+++ Teaching/ConsoleApps/Day2/Test.cs
@@ -11,10 +11,24 @@ namespace Day2
         public int m2;
         public int total;
         public string name;
+        public int avg;
 
-        public void Total()
+        
+        public void display()
         {
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter roll");
+            roll= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter m1 and m2");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            m2 = Convert.ToInt32(Console.ReadLine());
             total = m1 + m2;
+            avg = total / 2;
+            
+            
+
+
         }
 
 
@@ -27,20 +41,41 @@ namespace Day2
         {
 
             Student s1 = new Student();
-            Console.WriteLine("Enter Name");
-            s1.name = Console.ReadLine();
-            Console.WriteLine("Enter m1 and m2");
-            s1.m1 = Convert.ToInt32(Console.ReadLine());
-            s1.m2 = Convert.ToInt32(Console.ReadLine());
+            s1.display();
+            Student s2 = new Student();
+            s2.display();
+            Student s3 = new Student();
+            s3.display();
 
-            s1.total = s1.m1 + s1.m2;
+
+
 
             Console.WriteLine("\t\t Report\n");
             Console.WriteLine("\t\t____________");
             Console.WriteLine("Name          {0}",s1.name);
+            Console.WriteLine("roll          {0}", s1.roll);
             Console.WriteLine("M1             {0}",s1.m1);
             Console.WriteLine("m2              {0}",s1.m2);
             Console.WriteLine("Total              {0}", s1.total);
+
+            Console.WriteLine("\t\t Report\n");
+            Console.WriteLine("\t\t____________");
+            Console.WriteLine("Name          {0}", s2.name);
+            Console.WriteLine("roll          {0}", s2.roll);
+            Console.WriteLine("M1             {0}", s2.m1);
+            Console.WriteLine("m2              {0}", s2.m2);
+            Console.WriteLine("Total              {0}", s2.total);
+
+            Console.WriteLine("\t\t Report\n");
+            Console.WriteLine("\t\t____________");
+            Console.WriteLine("Name          {0}", s3.name);
+            Console.WriteLine("roll          {0}", s3.roll);
+            Console.WriteLine("M1             {0}", s3.m1);
+            Console.WriteLine("m2              {0}", s3.m2);
+            Console.WriteLine("Total              {0}", s3.total);
+
+
+
 
 
             Console.ReadKey();
