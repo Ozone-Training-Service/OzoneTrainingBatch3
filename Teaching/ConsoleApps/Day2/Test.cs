573bc22a5389efe5f5d4df90c6bd8758e4ad4dfc
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-17 10:44:43 +0530

    Student object

---
+++ Teaching/ConsoleApps/Day2/Test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    class Student
+    {
+        public int roll;
+        public int m1;
+        public int m2;
+        public int total;
+        public string name;
+
+        public void Total()
+        {
+            total = m1 + m2;
+        }
+
+
+    }
+
+    class Test
+    {
+
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student();
+            Console.WriteLine("Enter Name");
+            s1.name = Console.ReadLine();
+            Console.WriteLine("Enter m1 and m2");
+            s1.m1 = Convert.ToInt32(Console.ReadLine());
+            s1.m2 = Convert.ToInt32(Console.ReadLine());
+
+            s1.total = s1.m1 + s1.m2;
+
+            Console.WriteLine("\t\t Report\n");
+            Console.WriteLine("\t\t____________");
+            Console.WriteLine("Name          {0}",s1.name);
+            Console.WriteLine("M1             {0}",s1.m1);
+            Console.WriteLine("m2              {0}",s1.m2);
+            Console.WriteLine("Total              {0}", s1.total);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Teaching/ConsoleApps/Day2/third.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day2
+{
+    class third
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("third");
+        }
+    }
+}
