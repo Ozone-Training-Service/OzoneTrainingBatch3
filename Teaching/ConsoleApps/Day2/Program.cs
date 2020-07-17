8c42f24a4be8a730691bd2cadc2153e1d8a1b62c
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-17 10:10:01 +0530

    Functions

--- Teaching/ConsoleApps/Day2/Program.cs
+++ Teaching/ConsoleApps/Day2/Program.cs
@@ -2,11 +2,63 @@
 
 namespace Day2
 {
+
+    class Sample
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+        public int GetMeARandomNum()
+        {
+            Random rn = new Random();
+
+            int num = rn.Next(100);
+
+
+            return num;
+        }
+
+
+        public void Sum(int x, int y)
+        {
+
+
+            Console.WriteLine("{0}+{1}={2}",x,y,(x+y));
+        }
+
+
+        public int Sub(int x, int y)
+        {
+
+
+            return (x - y);
+        }
+
+
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Sample o = new Sample();
+            o.Hello();
+
+            int x=o.GetMeARandomNum();
+           
+            
+            Console.WriteLine("Random num="+x);
+
+            o.Sum(10,10);
+
+            int ans = o.Sub(10,5);
+            Console.WriteLine("Sub="+ans);
+
+            Console.ReadKey();
         }
     }
 }
